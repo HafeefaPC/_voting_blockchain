e95d78e4acc7cf1220bab56b66a3cb0d93558120
Hafeefa <hafeefapc2003@gmail.com>
2024-03-26 00:10:18 +0530

    ..

--- client/contracts/votingcontract.sol
+++ client/contracts/votingcontract.sol
@@ -1,9 +1,11 @@
 //SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
-import "@openzeppelin/contracts/utils/Counters.sol";
+
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
 contract Create {
-    using Counters for Counters.Counters;
+    using Counters for Counters.Counter;
     Counters.Counter public _voterIds;
     Counters.Counter public _candidateIds;
     address public votingOrganizer;
@@ -17,27 +19,183 @@ contract Create {
         address _address;
         string ipfs;
     }
-event CandidateCreate(
-    uint256 indexed candidateId;
-        string age;
-        string name;
-        string image;
-        uint256 voteCount;
-        address _address;
-        string ipfs;
-)
-address[] public candidateAddress;
-mapping(address => candidate) public candidates;
 
+    event CandidateCreate(
+        uint256 indexed candidateId,
+        string age,
+        string name,
+        string image,
+        uint256 voteCount,
+        address _address,
+        string ipfs
+    );
 
-address[] public votedVoters;
-mapping(address => voter) public voters;
+    address[] public candidateAddress;
+    mapping(address => Candidate) public candidates;
 
-string Voter {
-    unint256 voter_voterId;
-    string voter_name;
-    string voter_image;
-     string voter_address;
-}
+    struct Voter {
+        uint256 voter_voterId;
+        string voter_name;
+        string voter_image;
+        string voter_address;
+        uint256 voter_allowed;
+        bool voter_voted;
+        uint256 voter_vote;
+        string voter_ipfs;
+    }
 
+    address[] public votedVoters;
+    mapping(address => Voter) public voters;
+
+    constructor() {
+        votingOrganizer = msg.sender;
+    }
+
+    function setCandidate(
+        address _address,
+        string memory _age,
+        string memory _name,
+        string memory _image,
+        string memory _ipfs
+    ) public {
+        require(
+            msg.sender == votingOrganizer,
+            "Only the organizer can add candidates"
+        );
+        _candidateIds.increment();
+        uint256 idNumber = _candidateIds.current();
+        Candidate storage candidate = candidates[_address];
+        candidate.age = _age;
+        candidate.name = _name;
+        candidate.candidateId = idNumber;
+        candidate.image = _image;
+        candidate.voteCount = 0;
+        candidate._address = _address;
+        candidate.ipfs = _ipfs;
+        candidateAddress.push(_address);
+        emit CandidateCreate(idNumber, _age, _name, _image, 0, _address, _ipfs);
+    }
+
+    function getCandidate() public view returns (address[] memory) {
+        return candidateAddress;
+    }
+
+    function getCandidateLength() public view returns (uint256) {
+        return candidateAddress.length;
+    }
+
+    function getCandidatedata(
+        address _address
+    )
+        public
+        view
+        returns (
+            string memory,
+            string memory,
+            uint256,
+            string memory,
+            uint256,
+            string memory,
+            address
+        )
+    {
+        return (
+            candidates[_address].age,
+            candidates[_address].name,
+            candidates[_address].candidateId,
+            candidates[_address].image,
+            candidates[_address].voteCount,
+            candidates[_address].ipfs,
+            candidates[_address]._address
+        );
+    }
+    function voterRight(
+        address _address,
+        string memory _name,
+        string memory _image,
+        string memory _ipfs
+    ) public {
+        require(
+            msg.sender == votingOrganizer,
+            "Only the organizer can add voters"
+        );
+        _voterIds.increment();
+        uint256 idNumber = _voterIds.current();
+        Voter storage voter = voters[_address];
+        require(voter.voter_allowed == 0);
+        voter.voter_allowed = 1;
+        voter.voter_name = _name;
+        voter.voter_image = _image;
+        voter.voter_address = _address;
+        voter.voter_voterId = idNumber;
+        voter.voter_voted = false;
+        voter.voter_vote = 1000;
+        voter.voter_ipfs = _ipfs;
+
+        votersAddress.push(_address);
+
+        emit VoterCreated(
+            idNumber,
+            _name,
+            _image,
+            _address,
+            voter.voter_allowed,
+            voter.voter_voted,
+            voter.voter_vote,
+            _ipfs
+        );
+    }
+
+    function vote(
+        address _candidateAddress,
+        uint256 _candidateVoteId
+    ) external {
+        Voter storage voter = voters[msg.sender];
+        require(!voter.voter_voted, "you have already voted");
+        require(voter.voter_allowed != 0, "you are no right to vote");
+
+        voter.voter_voted = true;
+        voter.voter_vote = _candidateVoteId;
+
+        votedVoters.push(msg.sender);
+
+        candidates[_candidateAddress].voteCount += voter.voter_allowed;
+    }
+    function getVoterLength() public view returns (uint256) {
+        return votersAddress.length;
+    }
+
+    function getVoterdata(
+        address _address
+    )
+        public
+        view
+        returns (
+            uint256,
+            string memory,
+            string memory,
+            address,
+            string memory,
+            uint256,
+            bool
+        )
+    {
+        return (
+            voters[_address].voter_voterId,
+            voters[_address].voter_name,
+            voters[_address].voter_image,
+            voters[_address].voter_address,
+            voters[_address].voter_ipfs,
+            voters[_address].voter_allowed,
+            voters[_address].voter_voted
+        );
+    }
+
+    function getVotedVoters() public view returns (address[] memory) {
+        return votedVoters;
+    }
+
+    function getVoterList() public view returns (address[] memory) {
+        return votersAddress;
+    }
 }
